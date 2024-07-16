bb2d1e8e6bd951dd2ff1e2c2ac0600cec53712c4
MrChrisBond <christopher.bond2@education.wa.edu.au>
2024-07-17 05:16:01 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,9 +6,6 @@
     <nav>
         <ul>
             <li><a href="../index.php">Home</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
         </ul>
     </nav>
 </header>
